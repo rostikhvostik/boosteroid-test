c5561ebbf3d39496569f86c178ca8f4f84ccd09d
Rostik Hvostik <4360000@ukr.net>
2021-02-17 01:01:44 +0200

    Add GET /users route

--- platform/routes/api.php
+++ platform/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\SongController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('song', SongController::class)->name('song');
+Route::get('users', UserController::class)->name('users');
