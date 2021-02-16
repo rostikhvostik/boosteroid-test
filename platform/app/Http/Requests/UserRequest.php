99d96040c0c1e9cd676672f93c29b7983f745fbd
Rostik Hvostik <4360000@ukr.net>
2021-02-17 00:47:05 +0200

    Add UserRequest

---
+++ platform/app/Http/Requests/UserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'operator' => ['sometimes', Rule::in(['<', '=', '>'])],
+            'duration' => ['sometimes', 'integer', 'min:0', 'not_in:0'],
+        ];
+    }
+}
