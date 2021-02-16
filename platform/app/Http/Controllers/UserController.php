05bad88a6cc537f4234ae219871f38461714b9ce
Rostik Hvostik <4360000@ukr.net>
2021-02-17 01:51:55 +0200

    Add Api Doc User

--- platform/app/Http/Controllers/UserController.php
+++ platform/app/Http/Controllers/UserController.php
@@ -9,6 +9,33 @@ use App\UseCases\User\Handler as UserHandler;
 
 class UserController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/users",
+     *     summary="",
+     *     tags={"Users"},
+     *     @OA\Parameter(
+     *         name="operator",
+     *         in="query",
+     *         required=false,
+     *         @OA\Schema(type="string", nullable=true, enum={"<", "=", ">"})
+     *     ),
+     *     @OA\Parameter(
+     *         name="total_duration",
+     *         in="query",
+     *         required=false,
+     *         @OA\Schema(type="integer", nullable=true)
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             ref="#/components/schemas/UserResourceCollection",
+     *         ),
+     *     ),
+     * )
+     */
     public function __invoke(UserRequest $request, UserHandler $handler)
     {
         return UserResource::collection(
--- platform/app/Http/Resources/UserResource.php
+++ platform/app/Http/Resources/UserResource.php
@@ -16,6 +16,13 @@ use Illuminate\Http\Resources\Json\JsonResource;
  */
 class UserResource extends JsonResource
 {
+    /**
+     * @OA\Schema(
+     *     schema="User",
+     *     @OA\Property(property="email", type="string"),
+     *     @OA\Property(property="total_duration", type="integer"),
+     * ),
+     */
     public function toArray($request)
     {
         return [
@@ -24,3 +31,107 @@ class UserResource extends JsonResource
         ];
     }
 }
+
+/**
+ * @OA\Schema(
+ *     schema="UserResourceCollection",
+ *     @OA\Property(
+ *         property="data",
+ *         type="array",
+ *         @OA\Items(ref="#/components/schemas/User"),
+ *     ),
+ *     @OA\Property(
+ *         property="links",
+ *         type="object",
+ *         allOf={
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                      property="first",
+ *                      type="string",
+ *                      description="Link to the first page",
+ *                      example="/api/users?page=1"
+ *                 ),
+ *                 @OA\Property(
+ *                      property="last",
+ *                      type="string",
+ *                      description="Link to the last page",
+ *                      example="/api/users?page=100"
+ *                 ),
+ *                 @OA\Property(
+ *                      property="next",
+ *                      type="string",
+ *                      description="Link to the next page",
+ *                      example="/api/users?page=2"
+ *                 ),
+ *                 @OA\Property(
+ *                      property="prev",
+ *                      type="string",
+ *                      description="Link to the previous page",
+ *                      example=null
+ *                 )
+ *             )
+ *         },
+ *     ),
+ *     @OA\Property(
+ *         property="meta",
+ *         type="object",
+ *         allOf={
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                      property="current_page",
+ *                      type="integer",
+ *                      description="Current page number",
+ *                      example=1
+ *                 ),
+ *                 @OA\Property(
+ *                      property="from",
+ *                      type="integer",
+ *                      description="From element index on page",
+ *                      example=1
+ *                 ),
+ *                 @OA\Property(
+ *                      property="last_page",
+ *                      type="integer",
+ *                      description="Last page number",
+ *                      example=100
+ *                 ),
+ *                 @OA\Property(
+ *                     property="links",
+ *                     type="array",
+ *                     @OA\Items(
+ *                         type="object",
+ *                         description="Link",
+ *                         @OA\Property(property="active", type="boolean", example=false),
+ *                         @OA\Property(property="label", type="string", enum={"Previous", 1, "Next"}, default="Previous"),
+ *                         @OA\Property(property="url", type="string", example="/api/users?page=1"),
+ *                     )
+ *                 ),
+ *                 @OA\Property(
+ *                      property="path",
+ *                      type="string",
+ *                      description="Entity base path",
+ *                      example="/api/users"
+ *                 ),
+ *                 @OA\Property(
+ *                      property="per_page",
+ *                      type="integer",
+ *                      description="Elements number on one page",
+ *                      example=100
+ *                 ),
+ *                 @OA\Property(
+ *                      property="to",
+ *                      type="integer",
+ *                      description="To element index on page",
+ *                      example=100
+ *                 ),
+ *                 @OA\Property(
+ *                      property="total",
+ *                      type="integer",
+ *                      description="Total elements count",
+ *                      example=10000
+ *                 )
+ *             )
+ *         },
+ *     ),
+ * )
+ */
