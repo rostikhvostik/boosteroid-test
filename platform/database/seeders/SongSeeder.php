66248a7ff396e46b6d6391293d47ce629d9f3f5c
Rostik Hvostik <4360000@ukr.net>
2021-02-17 01:54:02 +0200

    Add SongSeeder

--- platform/database/seeders/SongSeeder.php
+++ platform/database/seeders/SongSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Song;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class SongSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(SongSeeder::class);
+        Song::factory(10)->create();
     }
 }
